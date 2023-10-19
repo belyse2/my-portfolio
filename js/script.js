7ffb388f2d8e4313dfae744cd84cdf17d15b7480
Belyse ISHIMWE <belyse2ishimwe@gmail.com>
2023-10-19 21:24:27 +0200

    JavaScript to highlight menu items on scroll

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true,
 });
+// JavaScript to highlight menu items on scroll
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.section');
+    const asideLinks = document.querySelectorAll('.aside .nav li a');
+  
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionBottom = sectionTop + section.clientHeight;
+  
+      if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+        asideLinks[index].classList.add('active');
+      } else {
+        asideLinks[index].classList.remove('active');
+      }
+    });
+  });
+  
